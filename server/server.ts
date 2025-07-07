792e6e05d8fc80fe84a09a541511dd978007fb33
ruska <ruska35@mail.ru>
2025-07-07 19:47:05 +0300

    post shorten temp

--- server/server.ts
+++ server/server.ts
@@ -1,22 +1,37 @@
 import express, { Request, response, Response } from "express";
 import bodyParser from "body-parser";
-import { getTesting } from "./requests";
+import cors from 'cors';
 
 const app = express();
 const port = 3000;
+
+app.use(cors({
+  origin: 'http://localhost:4444', // или '*' для всех (НЕ для продакшена)
+  methods: ['GET', 'POST'],
+  credentials: true, // если отправляешь куки или auth-заголовки
+}));
+
+app.use(express.json());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req: Request, res: Response) => {
-    res.send("Hello, TypeScript world!");
+  res.send("Hello, TypeScript world!");
 });
 
-getTesting(10).then((response) => {
-  console.log('response >> ', response);
-})
+app.post("/shorten", (req: Request, res: Response) => {
+  console.log('req >> ', req);
+  const { originalUrl, expiresAt, alias } = req.body;
+  
+  console.log('originalUrl >> ', originalUrl);
+  console.log('expiresAt >> ', expiresAt);
+  console.log('alias >> ', alias);
+
+  res.send({ shortUrl: '/urlll' });
+});
 
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
 
--- src/1-app/App.tsx
+++ src/1-app/App.tsx
@@ -1,9 +1,38 @@
+import { useEffect, useState } from 'react';
 import './styles/index.scss';
 
 export const App = () => {
+  const [shorted, setShorted] = useState('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:3000/shorten', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            originalUrl: '',
+            expiresAt: '',
+            alias: '',
+          })
+        });
+        const json = await response.json();
+        setShorted(json.shortUrl);
+      } catch (error) {
+        console.error('Ошибка при загрузке данных:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className={'App'}>
       <div className='container'>
+        {shorted}
+        
       </div>
     </div>
   );
